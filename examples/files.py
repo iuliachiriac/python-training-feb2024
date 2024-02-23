e6814df6ecc57049c5ad870ab2d8fe0c62030c17
Iulia Chiriac <iulia.chiriac@yahoo.com>
2024-02-23 11:31:28 +0200

    Add files

---
+++ examples/files.py
@@ -0,0 +1,15 @@
+with open("imports.py") as f:
+    for line in f:
+        print(line.rstrip())
+
+
+fruits = ['apples', 'oranges', 'pears', 'bananas', 'peaches', 'cherries']
+with open("out.txt", "w+") as f:
+    for fruit in fruits:
+        f.write(fruit)
+        f.write("\n")
+
+    print("After writing to file:", f.tell())
+    f.seek(0)
+    for line in f:
+        print(line, end='')
